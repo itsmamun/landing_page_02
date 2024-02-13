115abcbe9321d2a8e3303f2901a9dc3d2aebef87
itsmamun <mamunabdullaha65@gmail.com>
2024-02-13 12:47:37 +0600

    new

---
+++ app.js
@@ -0,0 +1,11 @@
+gsap.to("#nav",{
+    backgroundColor: "#000",
+    duration: 0.9,
+    height:"90px",
+    scrollTrigger: {
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+    }
+
+})
