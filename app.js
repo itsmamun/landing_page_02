b414b6e4ac92c7bcb42c251b400265b16ec82845
itsmamun <mamunabdullaha65@gmail.com>
2024-02-13 19:04:09 +0600

    new

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+    blur.style.left = dets.x -100 + "px"
+    blur.style.top = dets.y -100 + "px"
+})
+
+
 gsap.to("#nav",{
     backgroundColor: "#000",
     duration: 0.9,
-    height:"90px",
+    height:"80px",
     scrollTrigger: {
         trigger:"#nav",
         scroller:"body",
-        markers:true,
+        // markers:true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub:1
     }
 
 })
+
+gsap.to("#main" ,{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller: "body",
+        // markers:true 
+        start: "top -25%",
+        end: "top -70%",
+        scrub:2
+    }
+})
+
+
