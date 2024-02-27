257722784031f2a1e1ea429ae86df99e4085ac8a
itsmamun <mamunabdullaha65@gmail.com>
2024-02-27 17:19:16 +0600

    new

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var crsr = document.querySelector("#cursor")
 var blur = document.querySelector("#cursor-blur")
+
 document.addEventListener("mousemove", function(dets){
     crsr.style.left = dets.x + "px"
     crsr.style.top = dets.y + "px"
-    blur.style.left = dets.x -200 + "px"
-    blur.style.top = dets.y -200 + "px"
+    blur.style.left = dets.x - 200 + "px"
+    blur.style.top = dets.y - 200 + "px"
 })
 
 
 gsap.to("#nav",{
     backgroundColor: "#000",
-    duration: 0.9,
+    duration: 0.5,
     height:"80px",
     scrollTrigger: {
         trigger:"#nav",
@@ -18,10 +19,9 @@ gsap.to("#nav",{
         // markers:true,
         start: "top -10%",
         end: "top -11%",
-        scrub:1
-    }
-
-})
+        scrub:1,
+    },
+});
 
 gsap.to("#main" ,{
     backgroundColor: "#000",
@@ -31,8 +31,8 @@ gsap.to("#main" ,{
         // markers:true 
         start: "top -25%",
         end: "top -70%",
-        scrub:2
-    }
-})
+        scrub:2,
+    },
+});
 
 
